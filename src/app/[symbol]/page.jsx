c7b89a210f7f6cf6c9b20108cd92155a7f06a964
JaiSharma <sharmajai901@gmail.com>
2024-05-06 11:26:48 +0530

    changes

--- src/app/[symbol]/page.jsx
+++ src/app/[symbol]/page.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-import axios from "axios";
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'next/navigation';
 
-import Chart from "@/components/client/chart";
-import { env_variables } from "@/config";
+import Chart from '@/components/client/chart';
+import { env_variables } from '@/config';
 
-const Detail = async ({ params }) => {
-  const [companySymbol, exchange] = params.symbol.split(".") || [];
+const Detail = () => {
+  // const [companySymbol, exchange] = params.symbol.split('.') || [];
   const baseUrl = env_variables.base_api_url;
+  const [ratioData, setRatioData] = useState({});
+  const [company, setCompany] = useState('');
+  const [ratios,setRatios] = useState([])
 
   // const intraDayPromise = axios.get(
   //   `${baseUrl}/quote/intraday?symbol=${companySymbol}`
@@ -24,10 +29,19 @@ const Detail = async ({ params }) => {
   //   intraDayPromise,
   //   companyRatiosPromise,
   // ]);
+  const params = useParams();
+  useEffect(() => {
+    const [companySymbol, exchange] = params.symbol.split('.') || [];
+    setCompany(companySymbol);
+    const fetchData = async () => {
+      const ratio = await axios.get(`${baseUrl}/ratio?symbol=${params.symbol}`);
+      const ratioData = ratio.data.data;
+      setRatioData(ratioData);
+      setRatios(Object.keys(ratioData));
+    };
 
- const ratio = await axios.get(
-    `${baseUrl}/ratio?symbol=${params.symbol}`
-  );
+    fetchData();
+  }, []);
 
   // const presentTime = [];
 
@@ -53,43 +67,40 @@ const Detail = async ({ params }) => {
   //     label: intradayData.data.data.name,
   //     data: priceChartData,
   //   },
-  // ];
-
-  const ratioData = ratio.data.data;
-  const keys = Object.keys(ratioData);
+  // ]
 
   return (
-    <div className="w-full px-6 py-10">
-      <p className="mb-4 text-2xl font-bold">{companySymbol}</p>
+    <div className='w-full px-6 py-10'>
+      <p className='mb-4 text-2xl font-bold'>{company}</p>
       {/* <div className="flex gap-4"> */}
       {/*  <Chart data={chartData} /> */}
-        <div className="flex flex-wrap w-1/2 gap-2">
-          {keys.map((ratio, index) => {
-            return (
-              <div key={index} className="bg-gray-200 p-4 rounded w-[30%]">
-                <p className="font-bold pb-4 text-center">{ratio}</p>
-                <div className="flex justify-evenly text-center gap-2">
-                  <div>
-                    <p className="mb-4">2024</p>
-                    <p>{ratioData[ratio]["2024-03-31T00:00:00+00:00"]}</p>
-                  </div>
-                  <div>
-                    <p className="mb-4">2023</p>
-                    <p>{ratioData[ratio]["2023-03-31T00:00:00+00:00"]}</p>
-                  </div>
-                  <div>
-                    <p className="mb-4">2022</p>
-                    <p>{ratioData[ratio]["2022-03-31T00:00:00+00:00"]}</p>
-                  </div>
-                  <div>
-                    <p className="mb-4">2021</p>
-                    <p>{ratioData[ratio]["2021-03-31T00:00:00+00:00"]}</p>
-                  </div>
+      <div className='flex flex-wrap w-1/2 gap-2'>
+        {ratios.map((ratio, index) => {
+          return (
+            <div key={index} className='bg-gray-200 p-4 rounded w-[30%]'>
+              <p className='font-bold pb-4 text-center'>{ratio}</p>
+              <div className='flex justify-evenly text-center gap-2'>
+                <div>
+                  <p className='mb-4'>2024</p>
+                  <p>{ratioData[ratio]['2024-03-31T00:00:00+00:00']}</p>
+                </div>
+                <div>
+                  <p className='mb-4'>2023</p>
+                  <p>{ratioData[ratio]['2023-03-31T00:00:00+00:00']}</p>
+                </div>
+                <div>
+                  <p className='mb-4'>2022</p>
+                  <p>{ratioData[ratio]['2022-03-31T00:00:00+00:00']}</p>
+                </div>
+                <div>
+                  <p className='mb-4'>2021</p>
+                  <p>{ratioData[ratio]['2021-03-31T00:00:00+00:00']}</p>
                 </div>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
+      </div>
       {/* </div> */}
     </div>
   );
