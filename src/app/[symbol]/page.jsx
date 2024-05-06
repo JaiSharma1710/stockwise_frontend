1c036fd143c7a46ed9add268116f49e54e8fb26a
JaiSharma <sharmajai901@gmail.com>
2024-05-06 11:11:17 +0530

    changes

--- src/app/[symbol]/page.jsx
+++ src/app/[symbol]/page.jsx
@@ -8,49 +8,52 @@ const Detail = async ({ params }) => {
   const [companySymbol, exchange] = params.symbol.split(".") || [];
   const baseUrl = env_variables.base_api_url;
 
-  const intraDayPromise = axios.get(
-    `${baseUrl}/quote/intraday?symbol=${companySymbol}`
-  );
+  // const intraDayPromise = axios.get(
+  //   `${baseUrl}/quote/intraday?symbol=${companySymbol}`
+  // );
 
-  const companyDataPromise = axios.get(
-    `${baseUrl}/quote/current?symbol=${companySymbol}`
-  );
+  // const companyDataPromise = axios.get(
+  //   `${baseUrl}/quote/current?symbol=${companySymbol}`
+  // );
 
-  const companyRatiosPromise = axios.get(
+  // const companyRatiosPromise = axios.get(
+  //   `${baseUrl}/ratio?symbol=${params.symbol}`
+  // );
+
+  // const [intradayData, ratio] = await Promise.all([
+  //   intraDayPromise,
+  //   companyRatiosPromise,
+  // ]);
+
+ const ratio = await axios.get(
     `${baseUrl}/ratio?symbol=${params.symbol}`
   );
 
-  const [intradayData, companyData, ratio] = await Promise.all([
-    intraDayPromise,
-    companyDataPromise,
-    companyRatiosPromise,
-  ]);
+  // const presentTime = [];
 
-  const presentTime = [];
+  // const priceChartData = intradayData.data.data.grapthData
+  //   .map((ele) => {
+  //     const date = new Date(ele?.[0]);
+  //     const minute = date.getUTCMinutes();
+  //     const hour = date.getUTCHours();
+  //     if (presentTime.includes(`${hour}:${minute}`)) {
+  //       return false;
+  //     } else {
+  //       presentTime.push(`${hour}:${minute}`);
+  //     }
+  //     return {
+  //       secondary: ele?.[1],
+  //       primary: new Date(ele?.[0]),
+  //     };
+  //   })
+  //   .filter(Boolean);
 
-  const priceChartData = intradayData.data.data.grapthData
-    .map((ele) => {
-      const date = new Date(ele?.[0]);
-      const minute = date.getUTCMinutes();
-      const hour = date.getUTCHours();
-      if (presentTime.includes(`${hour}:${minute}`)) {
-        return false;
-      } else {
-        presentTime.push(`${hour}:${minute}`);
-      }
-      return {
-        secondary: ele?.[1],
-        primary: new Date(ele?.[0]),
-      };
-    })
-    .filter(Boolean);
-
-  const chartData = [
-    {
-      label: intradayData.data.data.name,
-      data: priceChartData,
-    },
-  ];
+  // const chartData = [
+  //   {
+  //     label: intradayData.data.data.name,
+  //     data: priceChartData,
+  //   },
+  // ];
 
   const ratioData = ratio.data.data;
   const keys = Object.keys(ratioData);
@@ -58,8 +61,8 @@ const Detail = async ({ params }) => {
   return (
     <div className="w-full px-6 py-10">
       <p className="mb-4 text-2xl font-bold">{companySymbol}</p>
-      <div className="flex gap-4">
-        <Chart data={chartData} />
+      {/* <div className="flex gap-4"> */}
+      {/*  <Chart data={chartData} /> */}
         <div className="flex flex-wrap w-1/2 gap-2">
           {keys.map((ratio, index) => {
             return (
@@ -87,7 +90,7 @@ const Detail = async ({ params }) => {
             );
           })}
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 };
